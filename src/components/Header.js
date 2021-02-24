1d5ba45e2ab5592d34d77c80c963e6f849a07703
manarm <mitchell@manarm.co>
2021-02-23 21:10:59 -0600

    Finish style nav, add headshot.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
 import React from "react";
 import {Link} from "gatsby";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 export default function Header() {
   return (
 <div className="nav-banner">
   <div className="container">
     <div className="navbar">  
-      <span class="brand"><Link to="/">Mitchell Manar</Link></span>
+      <span class="brand"><Link to="/">mitchell m</Link></span>
       <ul>
         <li><Link to="/">home</Link></li>
         <li><Link to="/tech">tech</Link></li>
         <li><Link to="/art">art</Link></li>
       </ul>
-      <div className="nav-icons">icons here</div>
+      <div className="nav-icons">
+        <a href="https://github.com/manarm" target="_blank" rel="noopener noreferrer" >
+          <FontAwesomeIcon className="icon" icon={faGithubAlt} />
+        </a>
+        <a href="https://twitter.com/itsmatchell" target="_blank" rel="noopener noreferrer" >
+          <FontAwesomeIcon className="icon" icon={faTwitter} />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+        </a>
+      </div>
     </div>
   </div>
 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import React from "react"
-
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
+import headshot from './headshot.jpg';
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello world!</div>
+      <div className="about-card">
+        <img className="headshot" height="200px" width="200px" src={headshot} alt="portrait of mitchell" />
+        Hello world!
+      </div>
     </Layout>
   )
 }
