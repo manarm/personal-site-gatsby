c2f75fc1b5d273ceb50b0b3f29d853d04f490aed
manarm <mitchell@manarm.co>
2021-02-25 11:08:03 -0600

    content responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import headshot from './headshot.jpg';
 export default function Home() {
   return (
     <Layout>
-      <div className="about-card">
-        <img className="headshot" height="200px" width="200px" src={headshot} alt="portrait of mitchell" />
+      <div className="about-card flex-wrap flex-md-nowrap justify-content-center">
+        <img className="headshot mb-2" height="200px" width="200px" src={headshot} alt="portrait of mitchell" />
         <div>
           <p><strong>Mitchell</strong> (he/him/his) is a fullstack developer and performing artist based in Chicago, IL. Check out some recent projects, or get in touch.</p>
           <p><strong>keywords:</strong></p>
