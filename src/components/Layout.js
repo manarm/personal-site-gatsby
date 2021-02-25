97e841d3f7c72073527b0d9f89447e5c48cdac57
manarm <mitchell@manarm.co>
2021-02-25 17:22:33 -0600

    didn't need bootstrap import

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "@fontsource/source-serif-pro" // Defaults to weight 400.
-import 'bootstrap';
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
