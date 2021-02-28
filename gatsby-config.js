f9e80664c8970bc8c929d5fe987b7901301e71a3
manarm <mitchell@manarm.co>
2021-02-27 22:35:54 -0600

    netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,8 @@ module.exports = {
       icon: `src/images/icon.png`, // This path is relative to the root of the site.
     },
   },
-  `gatsby-plugin-react-helmet`
+  `gatsby-plugin-react-helmet`,
+  `gatsby-plugin-netlify-cms`
 ],
   
 }
